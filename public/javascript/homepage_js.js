f8bd98ca0a5bf1198546a9da9d071973780c6b9d
Bhuwan-007 <chugh.bhuwan@gmail.com>
2025-10-03 19:02:35 +0530

    Adding all the files, first commit

---
+++ public/javascript/homepage_js.js
@@ -0,0 +1,30 @@
+const pokemonSprites = [
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png', // Pikachu
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',  // Bulbasaur
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',  // Charmander
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',  // Squirtle
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png', // Jigglypuff
+    ];
+
+const numberOfSprites = 10;
+
+for(let i=0; i<numberOfSprites; i++) {
+      const sprite = document.createElement('div');
+      sprite.classList.add('sprite');
+
+      const randomIndex = Math.floor(Math.random() * pokemonSprites.length);
+      sprite.style.backgroundImage = `url(${pokemonSprites[randomIndex]})`;
+
+      sprite.style.left = Math.random() * window.innerWidth + 'px';
+
+      const duration = 5 + Math.random() * 5;
+      sprite.style.animationDuration = `${duration}s`;
+
+      const size = 50 + Math.random() * 50;
+      sprite.style.width = `${size}px`;
+      sprite.style.height = `${size}px`;
+
+      sprite.style.animationDelay = `${Math.random() * 5}s`;
+
+      document.body.appendChild(sprite);
+    }
---
+++ routes.cjs
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
